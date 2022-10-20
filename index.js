ad6266ab0ba28503d2ac3c9b99e20da1550e7522
James Burnside <2684369+JamesBurnside@users.noreply.github.com>
2022-10-20 16:14:25 +0000

    Include Provider and theming in bundle

--- index.js
+++ index.js
@@ -2,33 +2,18 @@ import {
   Chat,
   Flex,
   Ref,
-  mergeStyles
+  mergeStyles,
+  Provider,
+  mergeThemes,
+  teamsTheme
 } from '@fluentui/react-northstar';
 
-const nStarBundle = {
+export {
   Chat,
   Flex,
   Ref,
-  mergeStyles
+  mergeStyles,
+  Provider,
+  mergeThemes,
+  teamsTheme
 };
-
-export default nStarBundle;
-
-/** ul
- * display: flex;
-    flex-direction: column;
-    list-style: none;
-    text-overflow: ellipsis;
-    flex: 1 1 auto;
-    margin: 0;
-    padding: 0.8rem 0.625rem 0.5rem 0.625rem;
-    overflow: auto;
-    height: 100%;
- */
-
-/** li
- * position: relative;
-    margin: 0;
-    padding: 0;
-    padding-top: 0.125rem;
- */
